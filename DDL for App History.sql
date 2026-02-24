91b1c824d3388640c90fcb9c8df2338f1bdf4235
jamirov-rd <jamirov@resourcedata.com>
2026-02-23 17:18:46 -0900

    DDL for App History updated

--- DDL for App History.sql
+++ DDL for App History.sql
@@ -4,14 +4,17 @@
 
 
 
--- NEPABPM.App.FormDropdownOption definition
+-- NEPABPM.App.FormDropdownOptionHistory definition
 
 -- Drop table
 
--- DROP TABLE NEPABPM.App.FormDropdownOption;
+-- DROP TABLE NEPABPM.App.FormDropdownOptionHistory;
 
-CREATE TABLE NEPABPM.App.FormDropdownOption (
-	FormDropdownOptionId int IDENTITY(1,1) NOT NULL,
+CREATE TABLE NEPABPM.App.FormDropdownOptionHistory (
+	FormDropdownOptionHistoryId int IDENTITY(1,1) NOT NULL,
+	HistoryDateFrom datetime NOT NULL,
+	HistoryDateTo datetime NULL,
+	FormDropdownOptionId int NOT NULL,
 	FkFormQuestionId int NOT NULL,
 	Display varchar(500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	ShortName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -22,25 +25,23 @@ CREATE TABLE NEPABPM.App.FormDropdownOption (
 	OrderNumber int NULL,
 	AlertMessage nvarchar(1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	IsActive bit NULL,
-	CONSTRAINT PK__FormDrop__EFA06AC36313C176 PRIMARY KEY (FormDropdownOptionId)
+	CONSTRAINT PK__FormDropdownOptionHistory__FormDropdownOptionHistoryId PRIMARY KEY (FormDropdownOptionHistoryId)
 );
 
 
--- NEPABPM.App.FormDropdownOption foreign keys
-
-ALTER TABLE NEPABPM.App.FormDropdownOption ADD CONSTRAINT FK__FormDropd__FkFor__162F4418 FOREIGN KEY (FkFormQuestionId) REFERENCES NEPABPM.App.FormQuestion(FormQuestionId);
 
 
 
-
-
--- NEPABPM.App.FormQuestion definition
+-- NEPABPM.App.FormQuestionHistory definition
 
 -- Drop table
 
--- DROP TABLE NEPABPM.App.FormQuestion;
+-- DROP TABLE NEPABPM.App.FormQuestionHistory;
 
-CREATE TABLE NEPABPM.App.FormQuestion (
+CREATE TABLE NEPABPM.App.FormQuestionHistory (
+	FormQuestionHistoryId int IDENTITY(1,1) NOT NULL,
+	HistoryDateFrom datetime NOT NULL,
+	HistoryDateTo datetime NULL,
 	FormQuestionId int NOT NULL,
 	QuestionText varchar(500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	InstructionsText varchar(500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -61,23 +62,26 @@ CREATE TABLE NEPABPM.App.FormQuestion (
 	NeedsBusinessReview bit NULL,
 	IsReEval bit NULL,
 	NotRequired bit NULL,
-	CONSTRAINT PK__FormQues__C751080734CA5622 PRIMARY KEY (FormQuestionId)
+	CONSTRAINT PK__FormQuestionHistory__FormQuestionHistoryId PRIMARY KEY (FormQuestionHistoryId)
 );
 
 
--- NEPABPM.App.FormQuestion foreign keys
+-- NEPABPM.App.FormQuestionHistory foreign keys
 
 
 
 
 
--- NEPABPM.App.FormSubSection definition
+-- NEPABPM.App.FormSubSectionHistory definition
 
 -- Drop table
 
--- DROP TABLE NEPABPM.App.FormSubSection;
+-- DROP TABLE NEPABPM.App.FormSubSectionHistory;
 
-CREATE TABLE NEPABPM.App.FormSubSection (
+CREATE TABLE NEPABPM.App.FormSubSectionHistory (
+	FormSubSectionHistoryId int IDENTITY(1,1) NOT NULL,
+	HistoryDateFrom datetime NOT NULL,
+	HistoryDateTo datetime NULL,
 	FormSubSectionId int NOT NULL,
 	FkFormSectionId int NOT NULL,
 	Heading varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -91,24 +95,27 @@ CREATE TABLE NEPABPM.App.FormSubSection (
 	CreatedBy varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	CreatedDate datetime NULL,
 	IsActive bit NOT NULL,
-	CONSTRAINT PK__FormSubS__DF84181BA54202F4 PRIMARY KEY (FormSubSectionId)
+	CONSTRAINT PK__FormSubSectionHistory__FormSubSectionHistoryId PRIMARY KEY (FormSubSectionHistoryId)
 );
 
 
--- NEPABPM.App.FormSubSection foreign keys
+-- NEPABPM.App.FormSubSectionHistory foreign keys
 
 
 
 
 
--- NEPABPM.App.FormSection definition
+-- NEPABPM.App.FormSectionHistory definition
 
 -- Drop table
 
--- DROP TABLE NEPABPM.App.FormSection;
+-- DROP TABLE NEPABPM.App.FormSectionHistory;
 
-CREATE TABLE NEPABPM.App.FormSection (
-	FormSectionId int IDENTITY(1,1) NOT NULL,
+CREATE TABLE NEPABPM.App.FormSectionHistory (
+	FormSectionHistoryId int IDENTITY(1,1) NOT NULL,
+	HistoryDateFrom datetime NOT NULL,
+	HistoryDateTo datetime NULL,
+	FormSectionId int NOT NULL,
 	Heading varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	OrderNumber int NOT NULL,
 	Instructions varchar(500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -116,26 +123,24 @@ CREATE TABLE NEPABPM.App.FormSection (
 	CreatedDate datetime NULL,
 	IsActive bit NOT NULL,
 	DocumentContext varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	CONSTRAINT PK__FormSect__3887B20A32F1041C PRIMARY KEY (FormSectionId)
+	CONSTRAINT PK__FormSectionHistory__FormSectionHistoryId PRIMARY KEY (FormSectionHistoryId)
 );
 
 
--- NEPABPM.App.FormSection foreign keys
-
-ALTER TABLE NEPABPM.App.FormSection ADD CONSTRAINT FormSection_DocumentWorkflowFormMap_FK FOREIGN KEY (DocumentContext) REFERENCES NEPABPM.App.DocumentWorkflowFormMap(FormDocumentContext);
 
 
 
-
-
--- NEPABPM.App.FormSubSectionQuestion definition
+-- NEPABPM.App.FormSubSectionQuestionHistory definition
 
 -- Drop table
 
--- DROP TABLE NEPABPM.App.FormSubSectionQuestion;
+-- DROP TABLE NEPABPM.App.FormSubSectionQuestionHistory;
 
-CREATE TABLE NEPABPM.App.FormSubSectionQuestion (
-	FormSubSectionQuestionId int IDENTITY(1,1) NOT NULL,
+CREATE TABLE NEPABPM.App.FormSubSectionQuestionHistory (
+	FormSubSectionQuestionHistoryId int IDENTITY(1,1) NOT NULL,
+	HistoryDateFrom datetime NOT NULL,
+	HistoryDateTo datetime NULL,
+	FormSubSectionQuestionId int NOT NULL,
 	FkFormSubSectionId int NOT NULL,
 	FkFormQuestionId int NOT NULL,
 	OrderNumber int NOT NULL,
@@ -146,16 +151,9 @@ CREATE TABLE NEPABPM.App.FormSubSectionQuestion (
 	RespectCheckboxFlag bit NULL,
 	DocumentContext varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	HideAllSectionsIfAnswerIs bit NULL,
-	CONSTRAINT PK__FormSubS__0B7A1654D6FBFB0A PRIMARY KEY (FormSubSectionQuestionId)
+	CONSTRAINT PK__FormSubSectionQuestionHistory__FormSubSectionQuestionHistoryId PRIMARY KEY (FormSubSectionQuestionHistoryId)
 );
 
-
--- NEPABPM.App.FormSubSectionQuestion foreign keys
-
-ALTER TABLE NEPABPM.App.FormSubSectionQuestion ADD CONSTRAINT FK_FormSubSectionQuestion_FormNumberingType FOREIGN KEY (FkFormNumberingTypeId) REFERENCES NEPABPM.App.FormNumberingType(FormNumberingTypeId);
-ALTER TABLE NEPABPM.App.FormSubSectionQuestion ADD CONSTRAINT FK_FormSubSectionQuestion_FormQuesiton FOREIGN KEY (FkFormQuestionId) REFERENCES NEPABPM.App.FormQuestion(FormQuestionId);
-ALTER TABLE NEPABPM.App.FormSubSectionQuestion ADD CONSTRAINT FK_FormSubSectionQuestion_FormSubSection FOREIGN KEY (FkFormSubSectionId) REFERENCES NEPABPM.App.FormSubSection(FormSubSectionId);
-ALTER TABLE NEPABPM.App.FormSubSectionQuestion ADD CONSTRAINT FK_FormSubSectionQuestion_SELF_FkParentId FOREIGN KEY (FkParentId) REFERENCES NEPABPM.App.FormSubSectionQuestion(FormSubSectionQuestionId);
 
 
 
