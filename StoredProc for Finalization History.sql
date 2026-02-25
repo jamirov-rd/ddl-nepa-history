bb0d7a05fc4a1acb446ffe90e8db900a6d97375c
jamirov-rd <jamirov@resourcedata.com>
2026-02-24 15:22:30 -0900

    Added stored proc to create history entries when Document is finalized

---
+++ StoredProc for Finalization History.sql
@@ -0,0 +1,141 @@
+CREATE OR ALTER PROCEDURE NEPABPM.Project.usp_FinalizeDocument
+    @DocumentId INT,
+    @FinalizedBy VARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Read ProjectId from Document
+        DECLARE @ProjectId INT;
+
+        SELECT @ProjectId = FkProjectId
+        FROM NEPABPM.Project.Document
+        WHERE DocumentId = @DocumentId;
+
+        IF @ProjectId IS NULL
+        BEGIN
+            RAISERROR('Document not found: %d', 16, 1, @DocumentId);
+            RETURN;
+        END
+
+        -- 1) Create DocumentFinalization
+        DECLARE @DocumentFinalizationId INT;
+
+        INSERT INTO NEPABPM.Project.DocumentFinalization (FkProjectId, FkDocumentId, FinalizedBy, FinalizationDate)
+        VALUES (@ProjectId, @DocumentId, @FinalizedBy, GETDATE());
+
+        SET @DocumentFinalizationId = SCOPE_IDENTITY();
+
+        -- 2) Copy FormQuestionAnswers -> FormQuestionAnswersHistory
+        DECLARE @QuestionAnswersMap TABLE (
+            OriginalFormQuestionAnswersId INT,
+            NewFormQuestionAnswersHistoryId INT
+        );
+
+        MERGE INTO NEPABPM.Project.FormQuestionAnswersHistory AS target
+        USING (
+            SELECT
+                fqa.FormQuestionAnswersId,
+                fqa.FkFormQuestionId,
+                fqa.IsSkipped,
+                fqa.YesNoAnswer,
+                fqa.TextAnswer,
+                fqa.CreatedBy,
+                fqa.CreatedDate,
+                fqa.UpdatedBy,
+                fqa.UpdatedDate,
+                fqa.TextAnswerUnformatted
+            FROM NEPABPM.Project.FormQuestionAnswers fqa
+            WHERE fqa.FkDocumentId = @DocumentId
+        ) AS source
+        ON 1 = 0
+        WHEN NOT MATCHED THEN
+            INSERT (
+                FkDocumentFinalizationId,
+                FormQuestionAnswersId,
+                FkFormQuestionId,
+                IsSkipped,
+                YesNoAnswer,
+                TextAnswer,
+                CreatedBy,
+                CreatedDate,
+                UpdatedBy,
+                UpdatedDate,
+                TextAnswerUnformatted
+            )
+            VALUES (
+                @DocumentFinalizationId,
+                source.FormQuestionAnswersId,
+                source.FkFormQuestionId,
+                source.IsSkipped,
+                source.YesNoAnswer,
+                source.TextAnswer,
+                source.CreatedBy,
+                source.CreatedDate,
+                source.UpdatedBy,
+                source.UpdatedDate,
+                source.TextAnswerUnformatted
+            )
+        OUTPUT source.FormQuestionAnswersId, inserted.FormQuestionAnswersHistoryId
+        INTO @QuestionAnswersMap (OriginalFormQuestionAnswersId, NewFormQuestionAnswersHistoryId);
+
+        -- 3) Copy FormDropdownAnswers -> FormDropdownAnswersHistory
+        INSERT INTO NEPABPM.Project.FormDropdownAnswersHistory (
+            FkFormQuestionAnswersHistoryId,
+            FormDropdownAnswersId,
+            FkFormQuestionAnswersId,
+            FkFormDropdownOptionId,
+            CreatedBy,
+            CreatedDate
+        )
+        SELECT
+            qam.NewFormQuestionAnswersHistoryId,
+            fda.FormDropdownAnswersId,
+            fda.FkFormQuestionAnswersId,
+            fda.FkFormDropdownOptionId,
+            fda.CreatedBy,
+            fda.CreatedDate
+        FROM NEPABPM.Project.FormDropdownAnswers fda
+        INNER JOIN @QuestionAnswersMap qam
+            ON qam.OriginalFormQuestionAnswersId = fda.FkFormQuestionAnswersId;
+
+        -- 4) Copy FormSubSectionAnswers -> FormSubSectionAnswersHistory
+        INSERT INTO NEPABPM.Project.FormSubSectionAnswersHistory (
+            FkDocumentFinalizationId,
+            FkFormSubSectionId,
+            SummaryAnswer,
+            CreatedBy,
+            CreatedDate,
+            UpdatedBy,
+            UpdatedDate,
+            SummaryTableImage,
+            SummaryAnswerUnformatted
+        )
+        SELECT
+            @DocumentFinalizationId,
+            fssa.FkFormSubSectionId,
+            fssa.SummaryAnswer,
+            fssa.CreatedBy,
+            fssa.CreatedDate,
+            fssa.UpdatedBy,
+            fssa.UpdatedDate,
+            fssa.SummaryTableImage,
+            fssa.SummaryAnswerUnformatted
+        FROM NEPABPM.Project.FormSubSectionAnswers fssa
+        WHERE fssa.FkDocumentId = @DocumentId;
+
+        COMMIT TRANSACTION;
+
+        SELECT @DocumentFinalizationId AS DocumentFinalizationId;
+
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
+        THROW;
+    END CATCH
+END;
